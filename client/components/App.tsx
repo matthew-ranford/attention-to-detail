695fede5541611fd509832ee0062f8d71ac50b20
Matthew Ranford <matt.ranford16@gmail.com>
2024-03-12 16:45:22 +1300

    Bootstrap imports added to index.tsx, Navbar component created, navigation imported to App component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,10 @@
+import Navigation from './Navbar'
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
+    <>
+      <Navigation />
+    </>
   )
 }
 
