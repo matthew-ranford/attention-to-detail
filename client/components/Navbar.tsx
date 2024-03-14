bbdc5a755bed107f02e63c6445e310a05aa9fdfc
Matthew Ranford <matt.ranford16@gmail.com>
2024-03-14 17:15:13 +1300

    Testing out styling and framer-motion

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -6,6 +6,28 @@ import { motion } from 'framer-motion'
 
 import logo from '../images/atd-logo.png'
 
+// TODO:
+// Look at changing the font-size of the li items
+
+const listVariants = {
+  hidden: {
+    y: -10,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1.8,
+      type: 'tween',
+      stiffness: 200,
+      damping: 20,
+    },
+  },
+}
+
 function Navigation() {
   return (
     <>
@@ -19,7 +41,7 @@ function Navigation() {
                   initial={{ y: -70, opacity: 0, scale: 0 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}
                   transition={{
-                    duration: 1.5,
+                    duration: 2,
                     type: 'spring',
                     stiffness: 200,
                     damping: 10,
@@ -35,7 +57,7 @@ function Navigation() {
                 </motion.div>{' '}
                 <motion.div
                   className="px-3"
-                  initial={{ y: -120, opacity: -1, scale: 0 }}
+                  initial={{ y: -50, opacity: -1, scale: 0 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}
                   transition={{
                     duration: 2.5,
@@ -51,10 +73,10 @@ function Navigation() {
                 </motion.div>{' '}
                 <motion.div
                   className="px-3"
-                  initial={{ y: -180, opacity: -1, scale: 0 }}
+                  initial={{ y: -150, opacity: -1, scale: 0 }}
                   animate={{ y: 0, opacity: 1, scale: 1 }}
                   transition={{
-                    duration: 3.2,
+                    duration: 2.8,
                   }}
                 >
                   <motion.span
@@ -67,44 +89,91 @@ function Navigation() {
                 </motion.div>
               </h1>
             </Navbar.Brand>
-            <Navbar.Toggle
-              aria-controls={`offcanvasNavbar-expand-${expand}`}
-              className="px-4"
+            <motion.div
+              transition={{ ease: 'backInOut', duration: 0.5 }}
+              whileHover={{
+                x: -3,
+              }}
             >
-              <i className="bi bi-justify display-6 text-light"></i>
-            </Navbar.Toggle>
+              <Navbar.Toggle
+                aria-controls={`offcanvasNavbar-expand-${expand}`}
+                className="px-4"
+              >
+                <i className="bi bi-justify display-6 text-light"></i>
+              </Navbar.Toggle>
+            </motion.div>
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
               style={{ width: '100%' }}
             >
-              <Offcanvas.Header closeButton className="background">
+              <Navbar.Toggle closeButton>
                 <Navbar.Brand>
-                  <img
-                    src={logo}
-                    className="img-fluid"
-                    style={{ height: '50px' }}
-                    alt="Company Logo"
-                  />
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    transition={{ ease: 'easeInOut', duration: 1.8 }}
+                    animate={{ opacity: 1 }}
+                    style={{ backgroundColor: 'black' }}
+                  >
+                    <h1 className="text-end text-light display-6 pe-1">
+                      Close
+                    </h1>
+                    <img
+                      src={logo}
+                      className="img-fluid"
+                      style={{ height: '200px' }}
+                      alt="Company Logo"
+                    />
+                  </motion.div>
                 </Navbar.Brand>
-              </Offcanvas.Header>
+              </Navbar.Toggle>
               <Offcanvas.Body className="text-center px-2 background">
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#about">
-                    <h2 className="text-light">About</h2>
+                  <Nav.Link href="#about" className="nav-link">
+                    <motion.div
+                      variants={listVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <li className="text-light display-6">About</li>
+                    </motion.div>
                   </Nav.Link>
-                  <Nav.Link href="#services">
-                    <h2 className="text-light">Services</h2>
+                  <Nav.Link href="#services" className="nav-link">
+                    <motion.div
+                      variants={listVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <li className="text-light display-6">Services</li>
+                    </motion.div>
                   </Nav.Link>
-                  <Nav.Link href="#projects">
-                    <h2 className="text-light">Projects</h2>
+                  <Nav.Link href="#projects" className="nav-link">
+                    <motion.div
+                      variants={listVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <li className="text-light display-6">Projects</li>
+                    </motion.div>
                   </Nav.Link>
-                  <Nav.Link href="#gallery">
-                    <h2 className="text-light">Gallery</h2>
+                  <Nav.Link href="#gallery" className="nav-link">
+                    <motion.div
+                      variants={listVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <li className="text-light display-6">Gallery</li>
+                    </motion.div>
                   </Nav.Link>
-                  <Nav.Link href="#contact">
-                    <h2 className="text-light">Contact</h2>
+                  <Nav.Link href="#contact" className="nav-link">
+                    <motion.div
+                      variants={listVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <li className="text-light display-6">Contact</li>
+                    </motion.div>
                   </Nav.Link>
                 </Nav>
               </Offcanvas.Body>
