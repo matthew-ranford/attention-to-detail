e093ece66ff3a578b775f5ccfd1f7c584c05b13d
Matthew Ranford <matt.ranford16@gmail.com>
2024-03-20 19:24:46 +1300

    Add company name to Footer component, add section with id of home to Carousel component to link back to top of page

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -7,44 +7,46 @@ import Ratio from 'react-bootstrap/Ratio'
 function CarouselBackground() {
   return (
     <>
-      <Carousel fade data-bs-theme="dark" controls={false} indicators={false}>
-        <Carousel.Item interval={3000}>
-          <Ratio aspectRatio="16x9">
-            <img
-              src="https://atd-decorators.co.nz/static/80070375_105547584557182_8749752288730975304_n-c16d4d1680e2a1eda4456ca8877f04d2.jpeg"
-              className="img-fluid"
-              alt=""
-            />
-          </Ratio>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <Ratio aspectRatio="16x9">
-            <img
-              className="img-fluid"
-              src="https://atd-decorators.co.nz/static/20210422_142201-e03303898ede8ce2fe0d4811632af8b3.jpg"
-              alt="First slide"
-            />
-          </Ratio>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <Ratio aspectRatio="16x9">
-            <img
-              className="img-fluid"
-              src="https://atd-decorators.co.nz/static/20210316_151036-8ed556b3d31fbcc3362ca47516170222.jpg"
-              alt="Second slide"
-            />
-          </Ratio>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <Ratio aspectRatio="16x9">
-            <img
-              className="img-fluid"
-              src="https://atd-decorators.co.nz/static/20210316_153408-f71593f2203bcf6095c70c178f5dd80a.jpg"
-              alt="Third slide"
-            />
-          </Ratio>
-        </Carousel.Item>
-      </Carousel>
+      <section id="home">
+        <Carousel fade data-bs-theme="dark" controls={false} indicators={false}>
+          <Carousel.Item interval={3000}>
+            <Ratio aspectRatio="16x9">
+              <img
+                src="https://atd-decorators.co.nz/static/80070375_105547584557182_8749752288730975304_n-c16d4d1680e2a1eda4456ca8877f04d2.jpeg"
+                className="img-fluid"
+                alt=""
+              />
+            </Ratio>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <Ratio aspectRatio="16x9">
+              <img
+                className="img-fluid"
+                src="https://atd-decorators.co.nz/static/20210422_142201-e03303898ede8ce2fe0d4811632af8b3.jpg"
+                alt="First slide"
+              />
+            </Ratio>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <Ratio aspectRatio="16x9">
+              <img
+                className="img-fluid"
+                src="https://atd-decorators.co.nz/static/20210316_151036-8ed556b3d31fbcc3362ca47516170222.jpg"
+                alt="Second slide"
+              />
+            </Ratio>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <Ratio aspectRatio="16x9">
+              <img
+                className="img-fluid"
+                src="https://atd-decorators.co.nz/static/20210316_153408-f71593f2203bcf6095c70c178f5dd80a.jpg"
+                alt="Third slide"
+              />
+            </Ratio>
+          </Carousel.Item>
+        </Carousel>
+      </section>
     </>
   )
 }
--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,39 +1,55 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { motion } from 'framer-motion'
 
 function Footer() {
   return (
-    <Container fluid="lg" className="mt-5 pt-5">
-      <Row className="text-center justify-content-center pb-3">
-        <Col xs={1}>
-          <a
-            href="https://www.facebook.com/attentiontodetaildecoratorsltd"
-            aria-label="link to facebook page"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bi bi-facebook display-6"></i>
-          </a>
-        </Col>
-        <Col xs={1}>
-          <a
-            href="https://www.instagram.com/attentiontodetail_decorators/"
-            aria-label="link to instagram page"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bi bi-instagram display-6"></i>
-          </a>
-        </Col>
-        <Col xs={1}>
-          <a href="tel: 027 312 9323" aria-label="telephone number">
-            <i className="bi bi-telephone display-6"></i>
-          </a>
+    <Container className="mt-5 pt-5">
+      <Row className="">
+        <Col xs={12} md={12}>
+          <h1 className="text-light">
+            <a
+              href="#home"
+              className="text-light"
+              style={{ textDecoration: 'none' }}
+            >
+              <motion.div>
+                <motion.span
+                  initial={{ opacity: 0, x: -150 }}
+                  transition={{ duration: 2 }}
+                  whileInView={{ opacity: 1, x: 0, color: '#f8cf40' }}
+                >
+                  A
+                </motion.span>
+                ttention
+              </motion.div>
+              <motion.div>
+                <motion.span
+                  initial={{ opacity: 0, x: -125 }}
+                  transition={{ duration: 2.5 }}
+                  whileInView={{ opacity: 1, x: 0, color: '#f8cf40' }}
+                >
+                  T
+                </motion.span>
+                o
+              </motion.div>
+              <motion.div>
+                <motion.span
+                  initial={{ opacity: 0, x: -100 }}
+                  transition={{ duration: 3 }}
+                  whileInView={{ opacity: 1, x: 0, color: '#f8cf40' }}
+                >
+                  D
+                </motion.span>
+                etail
+              </motion.div>
+            </a>
+          </h1>
         </Col>
       </Row>
-      <Row className="text-center justify-content-center pb-3 pt-2">
-        <Col className="text-light">
+      <Row className="text-center justify-content-center pt-2">
+        <Col className="text-light pb-2">
           <div>
             Made with ❤️ by
             <a
