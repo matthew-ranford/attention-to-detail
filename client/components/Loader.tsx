c0be90fd8a92601277fa47afa9928858ecbaff94
Matthew Ranford <matt.ranford16@gmail.com>
2024-04-12 12:01:02 +1200

    Changes to loader layout, nreplaced logo

--- client/components/Loader.tsx
+++ client/components/Loader.tsx
@@ -13,18 +13,17 @@ function Loader() {
         xPercent: '0',
         opacity: 0,
         duration: 1,
-        delay: 0.1,
       })
-        .from(['#title-0', '#title-1', '#title-2', '#title-3'], {
+        .from(['#title-logo'], {
           opacity: 0,
-          y: '+=15',
-          stagger: 0.5,
+          scale: 10,
         })
-        .to(['#title-0', '#title-1', '#title-2', '#title-3'], {
+        .to(['#title-logo'], {
           opacity: 0,
-          y: '-=15',
-          delay: 0.3,
-          stagger: 0.5,
+          scale: 1,
+          x: '-=250',
+          delay: 1.2,
+          duration: 0.7,
         })
         .to('#intro-slider', { xPercent: '-100', duration: 0.5 })
         .from('#main-layout', {
@@ -40,26 +39,17 @@ function Loader() {
       <div className="relative" ref={comp}>
         <div
           id="intro-slider"
-          className="h-screen p-10 bg-zinc-800 absolute z-10 w-full flex flex-col place-items-center gap-15 tracking-tight"
+          className="h-screen p-10 absolute z-10 w-full flex flex-col place-items-center gap-15 tracking-tight pt-5"
+          style={{ background: '#2b2b2b' }}
         >
           <img
-            id="title-0"
+            id="title-logo"
             src="client/images/atd-logo.png"
-            className="img-fluid"
+            className="img-fluid pt-5"
             alt="Company logo"
-            height={200}
-            width={200}
+            height={400}
+            width={400}
           />
-
-          <h1 className="display-4 text-zinc-200 mt-20" id="title-1">
-            Attention
-          </h1>
-          <h1 className="display-4 text-zinc-200" id="title-2">
-            To
-          </h1>
-          <h1 className="display-4 text-zinc-200" id="title-3">
-            Detail
-          </h1>
         </div>
         <div className="navbar-container">
           <Navigation />
