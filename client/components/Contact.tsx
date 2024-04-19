79654e334b58a47c6bc5b8690112d80ff66c64c4
Matthew Ranford <matt.ranford16@gmail.com>
2024-04-20 11:07:26 +1200

    Adding initial framer-motion properties

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -8,6 +8,8 @@ import Button from 'react-bootstrap/Button'
 import emailjs from '@emailjs/browser'
 import { useState, ChangeEvent, FormEvent } from 'react'
 
+import { motion } from 'framer-motion'
+
 function Contact() {
   const [formData, setFormData] = useState({
     name: '',
@@ -63,100 +65,123 @@ function Contact() {
     <>
       <section id="contact" className="container mt-5">
         <div className="text-center">
-          <h1 className="text-primary mt-3 pt-5">Contact us!</h1>
-          <p className="lead text-light pt-4">
-            Questions? Fill out the form to contact me directly!
-          </p>
-          <p className="lead text-light mb-4 pb-5 pt-4">
-            Alternatively, message or call Corey at one of the links below!
-          </p>
+          <motion.div
+            initial={{ opacity: 0, y: -8 }}
+            transition={{ duration: 0.5 }}
+            whileInView={{ y: 0, opacity: 1 }}
+          >
+            <h1 className="text-primary mt-3 pt-5">Contact us!</h1>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 8 }}
+            transition={{ duration: 0.5 }}
+            whileInView={{ y: 0, opacity: 1 }}
+          >
+            <p className="lead text-light pt-4">
+              Questions? Fill out the form to contact me directly!
+            </p>
+            <p className="lead text-light mb-4 pb-5 pt-4">
+              Alternatively, message or call Corey at one of the links below!
+            </p>
+          </motion.div>
         </div>
         <Container fluid="lg">
           <Row>
             <Col xs={12} md={6}>
-              <Form onSubmit={handleEmailSubmit}>
-                <FloatingLabel
-                  controlId="floatingName"
-                  label="Name"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    name="name"
-                    type="name"
-                    placeholder="Name"
-                    required
-                    value={formData.name}
-                    onChange={handleFormInputChange}
-                  />
-                </FloatingLabel>
-                <FloatingLabel
-                  controlId="floatingInput"
-                  label="Email address"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    name="email"
-                    type="email"
-                    placeholder="name@example.com"
-                    required
-                    value={formData.email}
-                    onChange={handleFormInputChange}
-                  />
-                </FloatingLabel>
-                <FloatingLabel
-                  controlId="floatingNumber"
-                  label="Phone"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    name="number"
-                    type="phone"
-                    placeholder="Number"
-                    required
-                    value={formData.number}
-                    onChange={handleFormInputChange}
-                  />
-                </FloatingLabel>
-                <FloatingLabel
-                  controlId="floatingSubjectMatter"
-                  label="Subject"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    name="subject"
-                    type="subject"
-                    placeholder="Subject Matter"
-                    required
-                    value={formData.subject}
-                    onChange={handleFormInputChange}
-                  />
-                </FloatingLabel>
-
-                <FloatingLabel
-                  controlId="floatingTextarea"
-                  label="Message"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    name="message"
-                    as="textarea"
-                    placeholder="Leave a comment here"
-                    required
-                    value={formData.message}
-                    onChange={handleFormInputChange}
-                    style={{ height: '10rem' }}
-                  />
-                </FloatingLabel>
-                <div className="text-center">
-                  <Button
-                    variant="primary"
-                    className="px-3 py-2 mb-3 mt-3"
-                    type="submit"
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 1.5 }}
+                whileInView={{ opacity: 1 }}
+              >
+                <Form onSubmit={handleEmailSubmit}>
+                  <FloatingLabel
+                    controlId="floatingName"
+                    label="Name"
+                    className="mb-3"
                   >
-                    Submit
-                  </Button>
-                </div>
-              </Form>
+                    <Form.Control
+                      name="name"
+                      type="name"
+                      placeholder="Name"
+                      required
+                      value={formData.name}
+                      onChange={handleFormInputChange}
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Email address"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      name="email"
+                      type="email"
+                      placeholder="name@example.com"
+                      required
+                      value={formData.email}
+                      onChange={handleFormInputChange}
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId="floatingNumber"
+                    label="Phone"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      name="number"
+                      type="phone"
+                      placeholder="Number"
+                      required
+                      value={formData.number}
+                      onChange={handleFormInputChange}
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId="floatingSubjectMatter"
+                    label="Subject"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      name="subject"
+                      type="subject"
+                      placeholder="Subject Matter"
+                      required
+                      value={formData.subject}
+                      onChange={handleFormInputChange}
+                    />
+                  </FloatingLabel>
+
+                  <FloatingLabel
+                    controlId="floatingTextarea"
+                    label="Message"
+                    className="mb-3"
+                  >
+                    <Form.Control
+                      name="message"
+                      as="textarea"
+                      placeholder="Leave a comment here"
+                      required
+                      value={formData.message}
+                      onChange={handleFormInputChange}
+                      style={{ height: '10rem' }}
+                    />
+                  </FloatingLabel>
+                  <motion.div
+                    className="text-center"
+                    initial={{ opacity: 0, y: 8 }}
+                    transition={{ duration: 0.5 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                  >
+                    <Button
+                      variant="primary"
+                      className="px-3 py-2 mb-3 mt-3"
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                  </motion.div>
+                </Form>
+              </motion.div>
             </Col>
 
             <Col className="text-center pt-5">
