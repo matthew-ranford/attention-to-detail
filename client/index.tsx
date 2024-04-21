42497fda4d9dc7ce115a43f1d60fcd65f5ce346a
Matthew Ranford <matt.ranford16@gmail.com>
2024-04-21 15:20:34 +1200

    Primary colour text change

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '/client/css/main.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
