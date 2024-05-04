c7d28539c4c9b205111caa8ecbc106c9ec892b9a
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-05 08:52:37 +1200

    Change base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/attention-to-detail',
+  base: '/',
   build: {
     outDir: 'build',
     emptyOutDir: true,
