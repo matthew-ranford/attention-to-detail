c8542e39d6028ec307c08ad061619edc648ad95f
Matthew Ranford <matt.ranford16@gmail.com>
2025-02-19 14:08:43 +1300

    Working on navbar and hero

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,30 +1,59 @@
-import Carousel from 'react-bootstrap/Carousel'
+import hero from '../images/hero-section-atd-test.png'
+import logo from '../images/logo.png'
 
-import carouselImages from '../carouselData'
+import { Container } from 'react-bootstrap'
 
 function CarouselBackground() {
   return (
     <>
       <section id="home">
-        <Carousel
-          fade={true}
-          controls={false}
-          indicators={false}
-          interval={2500}
-          pause={false}
-        >
-          {carouselImages.map((image) => {
-            return (
-              <Carousel.Item key={image.id}>
-                <img
-                  src={image.image}
-                  className="d-block w-100"
-                  alt={'Painting & decorating image' + image.id}
-                />
-              </Carousel.Item>
-            )
-          })}
-        </Carousel>
+        <div className="relative">
+          <Container fluid="lg">
+            <img
+              src={logo}
+              alt="Attention To Detail Logo"
+              width="200px"
+              loading="eager"
+              decoding="async"
+              className="absolute mt-4 -mx-3 2xl:-mx-6"
+            />
+          </Container>
+          <img
+            src={hero}
+            alt="Attention To Detail Logo"
+            width="100%"
+            loading="eager"
+            decoding="async"
+            className="h-[800px] md:h-[750px] 2xl:h-[1500px] object-cover"
+          />
+          <div className="absolute inset-0 bg-[#006cc1] bg-opacity-10 sm:hidden"></div>
+        </div>
+        <Container fluid="lg">
+          <div className="absolute top-[25%] md:top-[25%] 2xl:top-[30%]">
+            <h1 className="font-bold header text-[3.2rem] md:text-6xl 2xl:text-7xl -ml-1 max-w-[350px] md:max-w-[665px] 2xl:max-w-[1200px]">
+              Elevate your space{' '}
+              <span className="secondary-header block mt-2 md:mt-4 2xl:pt-4 text-3xl md:text-[3.3rem] 2xl:text-[3.9rem]">
+                with Attention to Detail.
+              </span>
+            </h1>
+            <p className="roboto-paragraph text-lg md:text-xl 2xl:text-2xl max-w-[320px] md:max-w-[620px] 2xl:max-w-[700px] mt-4 2xl:pt-2">
+              At Attention to Detail Painting & Decorating services, we take
+              pride in making spaces look their best. Whether it’s a small
+              update or a big change, our team works carefully to bring your
+              ideas to life. Let us help you create a space you’ll love.
+            </p>
+            <div className="block sm:inline-block pt-4">
+              <button className="text-white w-[330px] sm:w-[200px] rounded bg-[#006cc1] transition-all duration-1000 hover:bg-[#006dc1a4] sm:mt-7 px-5 py-3">
+                Get a quote
+              </button>
+            </div>
+            <div className="block sm:inline-block mt-2">
+              <button className="w-[330px] sm:w-[200px] border rounded bg-[#C1C7C7] border-black transition-all duration-1000 hover:bg-[#a4a8a8] sm:mt-7 sm:ml-5 px-5 py-3">
+                Our services
+              </button>
+            </div>
+          </div>
+        </Container>
       </section>
     </>
   )
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 import { motion } from 'framer-motion'
 
-import logo from '../images/ATD-logo.png'
+import logo from '../images/logo.png'
 
 const listVariants = {
   hidden: {
@@ -30,28 +30,19 @@ const navLinks = [
 function Navigation() {
   return (
     <>
-      {['md'].map((expand) => (
-        <Navbar key={expand} expand={false} collapseOnSelect>
-          <Container fluid="lg">
-            <Navbar.Brand href="#">
-              <img
-                src={logo}
-                alt="Attention To Detail Logo"
-                width={250}
-                loading="eager"
-                decoding="async"
-                className="hidden sm:block"
-              />
-            </Navbar.Brand>
+      {['sm'].map((expand) => (
+        <Navbar key={expand} expand={'md'} collapseOnSelect>
+          <Container fluid="lg" className="pt-8">
+            <Navbar.Brand href="#"></Navbar.Brand>
             <motion.div>
               <Navbar.Toggle
                 aria-controls={`offcanvasNavbar-expand-${expand}`}
-                aria-label="navar toggle button"
+                aria-label="navbar toggle button"
                 className="px-4"
               >
                 <motion.i
                   className="bi bi-justify display-6"
-                  style={{ color: '#ffb700' }}
+                  style={{ color: '#006cc1' }}
                   whileHover={{ textShadow: '0px 0px 5px rgb(0, 0, 0)' }}
                 ></motion.i>
               </Navbar.Toggle>
@@ -65,25 +56,24 @@ function Navigation() {
               <Navbar.Toggle
                 aria-controls={`offcanvasNavbar-expand-${expand}`}
                 aria-label="navar toggle button"
-                style={{ backgroundColor: '#2b2b2b' }}
               >
                 <Navbar.Brand>
                   <motion.div
                     initial={{ opacity: 0 }}
                     transition={{ ease: 'easeInOut', duration: 1.8 }}
                     animate={{ opacity: 1 }}
-                    style={{ backgroundColor: '#2b2b2b' }}
+                    style={{ backgroundColor: '#FFF' }}
                   >
                     <motion.h1
-                      className="text-end display-6 pe-1"
-                      style={{ color: '#ffb700' }}
+                      className="text-end display-6 pt-4"
+                      style={{ color: '#006cc1' }}
                       whileHover={{
-                        textShadow: '0px 0px 5px #ffb700',
+                        textShadow: '0px 0px 5px ##005cc2',
                       }}
                     >
                       Close
                     </motion.h1>
-                    <motion.img
+                    {/* <motion.img
                       src={logo}
                       className="img-fluid "
                       style={{ height: '200px', margin: 'auto' }}
@@ -91,29 +81,49 @@ function Navigation() {
                       initial={{ y: -100, scale: 0 }}
                       transition={{ duration: 0.7 }}
                       animate={{ y: 0, scale: 1 }}
+                    /> */}
+
+                    <motion.img
+                      src={logo}
+                      alt="Attention To Detail Logo"
+                      width="400px"
+                      loading="eager"
+                      decoding="async"
+                      className="img-fluid"
+                      style={{ margin: 'auto' }}
+                      initial={{ y: -100, scale: 0 }}
+                      transition={{ duration: 0.7 }}
+                      animate={{ y: 0, scale: 1 }}
                     />
                   </motion.div>
                 </Navbar.Brand>
               </Navbar.Toggle>
-              <Offcanvas.Body
-                className="text-center px-2 background"
-                style={{ backgroundColor: '#272727' }}
-              >
-                <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Offcanvas.Body className="text-center px-2">
+                <Nav className="w-100 justify-content-end align-items-center gap-3">
                   {navLinks.map((link, index) => (
                     <Nav.Link key={index} href={link.href} className="nav-link">
                       <motion.div
                         variants={listVariants}
                         initial="hidden"
                         animate="visible"
-                        className="text-light"
+                        className="text-dark"
                       >
-                        <motion.li
-                          className="display-6"
-                          whileHover={{ scale: 1.02, opacity: 0.5 }}
-                        >
-                          {link.text}
-                        </motion.li>
+                        {link.text === 'Contact' ? (
+                          <motion.button
+                            className="text-white rounded bg-[#006cc1] transition-all duration-1000 hover:bg-[#006dc1a4] px-4 py-2"
+                            style={{ fontSize: '17px' }}
+                            whileHover={{ scale: 1.05 }}
+                          >
+                            {link.text}
+                          </motion.button>
+                        ) : (
+                          <motion.li
+                            style={{ fontSize: '17px' }}
+                            whileHover={{ scale: 1.02, opacity: 0.5 }}
+                          >
+                            {link.text}
+                          </motion.li>
+                        )}
                       </motion.div>
                     </Nav.Link>
                   ))}
